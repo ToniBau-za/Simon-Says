9cea20a6d3741b3378f4a8825bbb27b59586cc6f
tonibauza <tonibauza@alumnat.iesdamiahuguet.net>
2024-02-21 13:35:04 +0100

    Commit enseña bloque de color aleatorio hecho

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
     Mostra la notificació al panell notification
     Mostra el color hexadecimal passat per paràmetre
  */
-
+//
 function showNotification(message) {
     console.log(message)
     var notification = document.getElementById('notification');
-    notification.style.backgroundColor=message
+    notification.style.backgroundColor = colores[Math.trunc(Math.random()*colores.length)]
+
     notification.style.display = 'block';
 
-    setTimeout(function() {
+    setTimeout(function () {
         notification.style.display = 'none';
     }, 2000);
 }
@@ -20,19 +21,24 @@ let colores =['#ffd1dc', '#ffecd1', '#d1ffea', '#e3d1ff', '#ffbf80', '#ffef96','
 
 //el array de los colores iluminados
 //                               1            2           3           4           5           6           7           8           9          10           11          12         13          14            15         16
-let colorbrillante=['#F86C91FF','#FFB947FF','#42FFA9FF','#9241FFFF','#FF9228FF','#FFDF1CFF','#67FF1CFF','#27FFC1FF','#FF1FD9FF','#6839FFFF','#A82222FF','#29E3E3FF','#D6287FFF','#B9FF27FF','#FFD133FF','#21FF91FF']
+// let colorbrillante=['#F86C91FF','#FFB947FF','#42FFA9FF','#9241FFFF','#FF9228FF','#FFDF1CFF','#67FF1CFF','#27FFC1FF','#FF1FD9FF','#6839FFFF','#A82222FF','#29E3E3FF','#D6287FFF','#B9FF27FF','#FFD133FF','#21FF91FF']
 
 //los colores que se iran añadiendo mientras pase el juego
 let colorjuego=[]
 
-function iniciarPartida(){
-
-}
-
-/*
-Iniciar partida. Es posa 'async' davant per indicar que té
-comandes que gestionen el temps d'execució. Pot haver "esperes"
-*/
+//
+// function IniciarPartida(){
+//     i=colores[Math.trunc(Math.random()*colores.length)]
+//     colorjuego.push[i]
+//     console.log(colorjuego)
+// }
+// i=colores[Math.trunc(Math.random()*colores.length)]
+// colorjuego.push[i]
+// console.log(colorjuego)
+// /*
+// Iniciar partida. Es posa 'async' davant per indicar que té
+// comandes que gestionen el temps d'execució. Pot haver "esperes"
+// */
 // async function iniciarPartida(){
 //     for (let i=0;i<colores.length;i++){
 //         //TODO instruccions per mostrar el color. Per exemple
