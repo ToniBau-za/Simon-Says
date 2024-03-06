a28eb59861e9a2a11ec1adb8980cd072baed167b
tonibauza <tonibauza@alumnat.iesdamiahuguet.net>
2024-03-06 14:05:37 +0100

    Commit muestra sequencia hecho

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+
+
+
+//el array del principio del juego con los colores identificados
+//                         1          2          3          4           5         6         7         8         9        10         11        12        13        14        15        16
+let colores =['#ffd1dc', '#ffecd1', '#d1ffea', '#e3d1ff', '#ffbf80', '#ffef96','#aaff80','#96ffe0','#ff9cf0','#d1c2ff','#b2b2b2','#99e6e6','#d98cb3','#d6ff80','#ffe080','#80ffbf']
+
+//la sequencia de colores que se iran añadiendo mientras pase el juego
+let juegocolores=[]
+
+// los colores guardados del math.random
+let guardadoscolores =[]
+
+
+// /*
+// Iniciar partida. Es posa 'async' davant per indicar que té
+// comandes que gestionen el temps d'execució. Pot haver "esperes"
+// */
+async function iniciarPartida(){
+    sequencia()
+    for (let i=0;i<guardadoscolores.length;i++){
+        //TODO instruccions per mostrar el color. Per exemple
+        showNotification(guardadoscolores[i]) //aqui es donde se controla el que saldrá en el recuadro de sequencia
+        //Crida al mètode esperar
+        await esperar(2000);
+    }
+}
+function esperar(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
 /*
     Mostra la notificació al panell notification
     Mostra el color hexadecimal passat per paràmetre
@@ -6,49 +39,36 @@
 function showNotification(message) {
     console.log(message)
     var notification = document.getElementById('notification');
-    notification.style.backgroundColor = colores[Math.trunc(Math.random()*colores.length)]
-
+    notification.style.backgroundColor= message
     notification.style.display = 'block';
 
-    setTimeout(function () {
+    setTimeout(function() {
         notification.style.display = 'none';
     }, 2000);
 }
 
-//el array del principio del juego con los colores identificados
-//                         1          2          3          4           5         6         7         8         9        10         11        12        13        14        15        16
-let colores =['#ffd1dc', '#ffecd1', '#d1ffea', '#e3d1ff', '#ffbf80', '#ffef96','#aaff80','#96ffe0','#ff9cf0','#d1c2ff','#b2b2b2','#99e6e6','#d98cb3','#d6ff80','#ffe080','#80ffbf']
 
-//el array de los colores iluminados
-//                               1            2           3           4           5           6           7           8           9          10           11          12         13          14            15         16
-// let colorbrillante=['#F86C91FF','#FFB947FF','#42FFA9FF','#9241FFFF','#FF9228FF','#FFDF1CFF','#67FF1CFF','#27FFC1FF','#FF1FD9FF','#6839FFFF','#A82222FF','#29E3E3FF','#D6287FFF','#B9FF27FF','#FFD133FF','#21FF91FF']
+//elegir el color y guardarlo en el array de la sequencia que aparezca
 
-//los colores que se iran añadiendo mientras pase el juego
-let colorjuego=[]
+//crear la sequencia
+function sequencia(){
+    //TODO buscar manera de que detecte el color hexidecimal de la function
+    resultado = colores[Math.trunc(Math.random() * colores.length)]
+    guardadoscolores.push(resultado)
 
-//
-// function IniciarPartida(){
-//     i=colores[Math.trunc(Math.random()*colores.length)]
-//     colorjuego.push[i]
-//     console.log(colorjuego)
-// }
-// i=colores[Math.trunc(Math.random()*colores.length)]
-// colorjuego.push[i]
-// console.log(colorjuego)
-// /*
-// Iniciar partida. Es posa 'async' davant per indicar que té
-// comandes que gestionen el temps d'execució. Pot haver "esperes"
-// */
-// async function iniciarPartida(){
-//     for (let i=0;i<colores.length;i++){
-//         //TODO instruccions per mostrar el color. Per exemple
-//         showNotification(colors[i])
-//         //Crida al mètode esperar
-//         await esperar(2000);
-//     }
-// }
-// function esperar(milliseconds){
-//     return new Promise(resolve => {
-//         setTimeout(resolve, milliseconds);
-//     });
-// }
+}
+
+function Comprobacion(card) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
